4a7801860980d6a60a9b8c1b626664536109fc43
Nicolas Eldering <eldering.nicolas@gmail.com>
2016-04-27 12:45:17 -0400

    Update StorePrices.java

--- StorePrices.java
+++ StorePrices.java
@@ -6,7 +6,7 @@ public class StorePrices
    {
       
       double[] total, price, amount;
-      final arrayLength = 10;
+      final int arrayLength = 10;
       
       Scanner kb = new Scanner(System.in);
       
@@ -14,9 +14,36 @@ public class StorePrices
       price = new double[arrayLength];
       amount = new double[arrayLength];
       
+      System.out.println("This program will first take the price of 10 objects.");
+      System.out.println("Then will ask you for the amount of each of the 10 objects you purchased");
+      System.out.println("Finally it will calculate the total price for each object and print out a table\n");
+      
+      System.out.println("Please enter the prices of your objects.");
       
       for (int i=0; i<price.length; i++) {
          System.out.print("Enter the price of object " + (i+1)+ " here: ");
          price[i] = kb.nextDouble();
          
       }
+      System.out.println();
+      System.out.println("Please enter the amount of each object.");
+      
+      for (int i=0; i<amount.length; i++) {
+         System.out.print("Enter the amount of object " + (i+1)+ " here: ");
+         amount[i] = kb.nextDouble();
+         
+      }
+      
+      for (int i=0; i<total.length; i++) {
+         total[i] = price[i] * amount[i];
+      }
+      System.out.println();
+      System.out.println("  Total         Price        Amount   ");
+      System.out.println("  =====         =====        ======   ");
+      
+      for (int i=0; i<total.length; i++) {
+         System.out.printf(" $%5.2f     %8.2f     %8.0f   \n", total[i], price[i], amount[i]);
+       
+      }
+   }
+}
